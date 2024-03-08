bc5c9a1db7989ebc5dbf0d7604077c6fe26326cb
Bezalell Barnet W <bezbanz@gmail.com>
2024-03-08 18:14:04 +0300

    Update contant

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -48,7 +48,17 @@ const Portfolio = () => {
         </div>
         <div className='w-full h-full'>
           <Image
-            src='https://images.unsplash.com/photo-1469362102473-8622cfb973cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2419&q=80'
+            src='/project 6.jpg'
+            alt='/'
+            width='215'
+            height='217'
+            layout='responsive'
+            objectFit='cover'
+          />
+        </div>
+        <div className='w-full h-full'>
+          <Image
+            src='/project 8.jpg'
             alt='/'
             width='215'
             height='217'
